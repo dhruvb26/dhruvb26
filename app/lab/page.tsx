ba7a7c8f64175dfcfaff0da3b885eded4f42c15d
Dhruv Bansal <dk.bansal0026@gmail.com>
2026-04-08 00:13:30 -0700

    add description to lab and use card component

--- app/lab/page.tsx
+++ app/lab/page.tsx
@@ -5,6 +5,7 @@ import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function LabPage() {
 	const { signOut } = useClerk();
@@ -14,16 +15,17 @@ export default function LabPage() {
 			<div className="flex flex-col flex-1 bg-background">
 				<main className="flex flex-1 w-full max-w-3xl mx-auto md:ml-[15%] md:mr-auto flex-col items-start justify-between py-40 px-6 sm:px-10 md:px-16 bg-background">
 					<div className="w-full space-y-8">
-						<h1 className="text-2xl font-medium">Lab</h1>
+						<div className="flex flex-col items-start gap-2">
+							<h1 className="text-2xl font-medium">Lab</h1>
+							<p className="text-muted-foreground">Stuff</p>
+						</div>
 						<div className="grid grid-cols-3 gap-2">
 							<Link href="/lab/board">
-								<div className="border border-border p-4 rounded-md hover:bg-muted transition-colors">
-									Mood Board
-								</div>
+								<Card className="p-4 hover:bg-muted transition-colors text-base">Mood Board</Card>
 							</Link>
 						</div>
 					</div>
-					<div className="flex flex-col items-start gap-2">
+					<div className="flex flex-col items-start gap-1">
 						<Button
 							size="lg"
 							variant="link"
@@ -33,7 +35,7 @@ export default function LabPage() {
 							Home
 						</Button>
 						<Button size="lg" variant="link" hoverIcon={ChevronRight} onClick={() => signOut()}>
-							Sign Out
+							Logout
 						</Button>
 					</div>
 				</main>
